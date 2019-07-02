1b629adfe03839e91b0b08caa25e3f64f1e59f30
leonip <leonip2015@gmail.com>
2019-07-02 23:53:49 +0800

    initial commit Category Properties & Attributes

---
+++ app/src/main/java/com/leo/mvvm_test/dao/object/AttributesCategory.java
@@ -0,0 +1,14 @@
+package com.leo.mvvm_test.dao.object;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AttributesCategory {
+    @SerializedName("im:id")
+    public String id;
+    @SerializedName("term")
+    public String term;
+    @SerializedName("scheme")
+    public String scheme;
+    @SerializedName("label")
+    public String label;
+}
---
+++ app/src/main/java/com/leo/mvvm_test/dao/object/PropertiesCategory.java
@@ -0,0 +1,8 @@
+package com.leo.mvvm_test.dao.object;
+
+import com.google.gson.annotations.SerializedName;
+
+public class PropertiesCategory {
+    @SerializedName("attributes")
+    public AttributesCategory attributes;
+}
