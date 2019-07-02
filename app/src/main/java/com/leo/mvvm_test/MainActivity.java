f6d9fa31e6ab7942d69d0131e3f22fef75e7ebd6
leonip <leonip2015@gmail.com>
2019-07-02 23:22:59 +0800

    MainActivity - update UI,  add ViewModel

--- app/src/main/java/com/leo/mvvm_test/MainActivity.java
+++ app/src/main/java/com/leo/mvvm_test/MainActivity.java
@@ -1,7 +1,13 @@
 package com.leo.mvvm_test;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.leo.mvvm_test.recycler_view.FreeAppListRecyclerViewAdapter;
+import com.leo.mvvm_test.viewmodel.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainActivityViewModel model = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        model.init(this.getApplicationContext());
+
+        // init UI
+        RecyclerView bodyList = findViewById(R.id.bodyList);
+        bodyList.setLayoutManager(new LinearLayoutManager(this));
+
+        model.getUsers().observe(this, (res) -> {
+            bodyList.setAdapter(new FreeAppListRecyclerViewAdapter(res));
+            bodyList.setHasFixedSize(true);
+        });
     }
 }
