65d9171258bd96b2aedbd56178ed048d49c5b850
leonip <leonip2015@gmail.com>
2019-07-05 17:21:16 +0800

    change Recyclerview to listview

--- app/src/main/java/com/leo/mvvm_test/MainActivity.java
+++ app/src/main/java/com/leo/mvvm_test/MainActivity.java
@@ -3,11 +3,15 @@ package com.leo.mvvm_test;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
 
-import com.leo.mvvm_test.recycler_view.FreeAppListRecyclerViewAdapter;
+import com.leo.mvvm_test.dao.FreeAppEntry;
+import com.leo.mvvm_test.recycler_view.FreeAppListViewAdapter;
+import com.leo.mvvm_test.recycler_view.RecommendAppListRecyclerViewAdapter;
+import com.leo.mvvm_test.recycler_view.LocalRecyclerView;
 import com.leo.mvvm_test.viewmodel.MainActivityViewModel;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +24,28 @@ public class MainActivity extends AppCompatActivity {
         model.init(this.getApplicationContext());
 
         // init UI
-        RecyclerView bodyList = findViewById(R.id.bodyList);
-        bodyList.setLayoutManager(new LinearLayoutManager(this));
+        ListView bodyList = findViewById(R.id.bodyList);
+        ArrayList<FreeAppEntry> freeAppList = new ArrayList<>();
+        FreeAppListViewAdapter freeAppAdapter = new FreeAppListViewAdapter(this,freeAppList);
+        bodyList.setAdapter(freeAppAdapter);
 
-        model.getUsers().observe(this, (res) -> {
-            bodyList.setAdapter(new FreeAppListRecyclerViewAdapter(res));
-            bodyList.setHasFixedSize(true);
+        LocalRecyclerView rv = new LocalRecyclerView(this);
+        ArrayList<FreeAppEntry> recommendAppList = new ArrayList<>();
+        RecommendAppListRecyclerViewAdapter recommendAdapter = new RecommendAppListRecyclerViewAdapter(recommendAppList);
+        rv.setAdapter(recommendAdapter);
+        bodyList.addHeaderView(rv.getView());
+
+
+        model.getFreeApps().observe(this, (res) -> {
+            freeAppList.addAll(res);
+            freeAppAdapter.notifyDataSetChanged();
         });
+
+
+        model.getRecommendApps().observe(this, (res) -> {
+            recommendAppList.addAll(res);
+            recommendAdapter.notifyItemRangeInserted(0,res.size());
+        });
+
     }
 }
---
+++ app/src/main/java/com/leo/mvvm_test/recycler_view/FreeAppListViewAdapter.java
@@ -0,0 +1,77 @@
+package com.leo.mvvm_test.recycler_view;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.leo.mvvm_test.R;
+import com.leo.mvvm_test.dao.FreeAppEntry;
+import com.leo.mvvm_test.dao.object.PropertiesImage;
+import com.leo.mvvm_test.util.image.CircleTransform;
+import com.leo.mvvm_test.util.image.RoundedCornersTransformation;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+import java.util.ArrayList;
+
+public class FreeAppListViewAdapter extends ArrayAdapter<FreeAppEntry> {
+    private ArrayList<FreeAppEntry> mDataset;
+
+    // Constructor
+    public FreeAppListViewAdapter(Context context, ArrayList<FreeAppEntry> dataset) {
+        super(context, 0, dataset);
+        this.mDataset = dataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            convertView = inflater.inflate(R.layout.cell_free_app, parent, false);
+            viewHolder.txtPosition = convertView.findViewById(R.id.txtPosition);
+            viewHolder.txtTitle = convertView.findViewById(R.id.txtTitle);
+            viewHolder.txtType = convertView.findViewById(R.id.txtType);
+            viewHolder.txtRating = convertView.findViewById(R.id.txtRating);
+            viewHolder.imgIcon = convertView.findViewById(R.id.imgIcon);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        FreeAppEntry appInfo = mDataset.get(position);
+        String index = Integer.toString(position+1);
+        viewHolder.txtPosition.setText(index);
+        viewHolder.txtTitle.setText(appInfo.name.label);
+        viewHolder.txtType.setText(appInfo.category.attributes.label);
+        viewHolder.txtRating.setText("70");
+
+        PropertiesImage img = appInfo.image.get(0);
+        int length = Integer.parseInt(img.attributes.height);
+        Transformation transformType = position%2 == 1 ?
+                new CircleTransform() :
+                new RoundedCornersTransformation(10, 0);
+        Picasso.get()
+                .load(img.label)
+                .transform(transformType)
+                .resize(length,length).into(viewHolder.imgIcon);
+
+        return convertView;
+    }
+
+    private static class ViewHolder{
+        public TextView txtPosition;
+        public TextView txtTitle;
+        public TextView txtType;
+        public TextView txtRating;
+        public ImageView imgIcon;
+    }
+}
